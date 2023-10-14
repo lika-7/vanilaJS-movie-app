358a026e466df755ceb47684f15f76ef5542f4c3
lika-7 <dhwhdtn93@gmail.com>
2023-10-14 18:20:57 +0900

    fetch 에러로 serverless 기능이 실행이 안되어 serverless 기능 수정, tsconfig 컴파일 옵션도 node.js환경으로 수정

--- api/movie.ts
+++ api/movie.ts
@@ -1,4 +1,4 @@
-import fetch from 'node-fetch' //node 환경에서 실행 할거고, node환경에서는 fetch 함수가 없어서 필요함
+import axios  from 'axios' //node 환경에서 실행 할거고, node환경에서는 fetch 함수가 없어서 필요함
 import { VercelRequest, VercelResponse } from '@vercel/node'
 const {APIKEY} = process.env
 
@@ -7,7 +7,9 @@ export default async function handler(request:VercelRequest, response:VercelResp
     const url = id 
         ?`https://www.omdbapi.com/?apikey=${APIKEY}&i=${id}&plot=full`
         :`https://www.omdbapi.com/?apikey=${APIKEY}&s=${title}&page=${page}`
-    const res = await fetch(url)
-    const json = await res.json()
+    const res = await axios.get(url)
+    //const json = await res.json()
+    //response.status(200).json(json)
+    const json = await res.data
     response.status(200).json(json)
 }
