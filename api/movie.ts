7033045a0dd8feb34710d5345979ee18cf125130
lika-7 <dhwhdtn93@gmail.com>
2023-10-14 15:33:59 +0900

    9. vercel serverless 기능 typescript 적용

--- api/movie.ts
+++ api/movie.ts
@@ -1,8 +1,8 @@
 import fetch from 'node-fetch' //node 환경에서 실행 할거고, node환경에서는 fetch 함수가 없어서 필요함
-
+import { VercelRequest, VercelResponse } from '@vercel/node'
 const {APIKEY} = process.env
 
-export default async function handler(request, response){
+export default async function handler(request:VercelRequest, response:VercelResponse){
     const {title, page, id} = JSON.parse(request.body)
     const url = id 
         ?`https://www.omdbapi.com/?apikey=${APIKEY}&i=${id}&plot=full`
