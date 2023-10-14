04fd7529c28d8d256230054d62d675bb4b5e1bb7
lika-7 <dhwhdtn93@gmail.com>
2023-10-14 14:46:47 +0900

    8 Search 및 MovieList 컴포넌트

--- src/components/Search.ts
+++ src/components/Search.ts
@@ -14,16 +14,16 @@ export default class Search extends Component{
         `
         //검색 기능이 필요, 데이터 입력 할 때, 버튼 누르거나 엔터키 눌렀을 때 실행되어야 함
         const inputEl = this.el.querySelector('input')
-        inputEl.addEventListener('input', ()=>{
+        inputEl?.addEventListener('input', ()=>{
             movieStore.state.searchText = inputEl.value
         })
-        inputEl.addEventListener('keydown', event=>{
+        inputEl?.addEventListener('keydown', event=>{
             if(event.key==='Enter' && movieStore.state.searchText.trim()){ //앞,뒤 공백의 제거이며, 공백을 제거했을 때 데이터가 없으면 false 있으면 true를 반환한다
                 searchMovies(1)
             }
         })
         const btnEl = this.el.querySelector('.btn')
-        btnEl.addEventListener('click', ()=>{
+        btnEl?.addEventListener('click', ()=>{
             if(movieStore.state.searchText.trim()){ //앞,뒤 공백의 제거이며, 공백을 제거했을 때 데이터가 없으면 false 있으면 true를 반환한다
                 searchMovies(1)
             }
